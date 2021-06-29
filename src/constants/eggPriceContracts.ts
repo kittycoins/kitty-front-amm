7c376e334c4b8589976b703d8ee8893f4372f4a7
Adam Stępnik <adam@excodus.com>
2021-06-29 16:06:12 +0200

    Initial commit

---
+++ src/constants/eggPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
+  cakeAddress: '0x98726fadc3EBc0e9006D5da04dB82c45876Ef464',
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019'
+}
+
+export default priceContracts
